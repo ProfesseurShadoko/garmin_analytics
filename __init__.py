6ccb403aff2122274066ead120420801df0f77f1
Jonas Wehrung-Montpezat <94191158+ProfesseurShadoko@users.noreply.github.com>
2025-09-28 14:08:08 +0200

    Add TODO for future metrics and lap analysis
    Added TODO items in __init__.py

--- __init__.py
+++ __init__.py
@@ -9,3 +9,13 @@ from .plot import *
 from .climb import ClimbSet
 
 Message(f"Garmin Activity Analysis ready to start!", "#")
+
+
+"""
+TODO:
+  - add metric: equivalent quantitiy in pasta for calories consumed
+  - add metric: equivalent quantity in pasta assuming 60g glucides/ hour
+  - add lap analysis
+  - add metric: number of km/h gained per W saved (corresponding time gain)
+  - add metric: number of W saved / kg saved
+"""
